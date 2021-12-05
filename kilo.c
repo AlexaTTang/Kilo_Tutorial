029abe9a8ff379ea1cb5d75f91a4ea00fa06045b
AlexaTTang <alexatamara00@gmail.com>
2021-12-05 22:16:35 +0000

    snap cursor to the end of line

--- kilo.c
+++ kilo.c
@@ -292,6 +292,7 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -299,7 +300,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
+      if (row && E.cx < row->size) {
         E.cx++;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
@@ -312,7 +315,13 @@ void editorMoveCursor(int key) {
       }
       break;
   }
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
+  }
 }
+
 
 void editorProcessKeypress() {
   int c = editorReadKey();
